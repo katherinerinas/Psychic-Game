b183c8b8e85dbe58bd770ab3e02e18ac77754de9
Katherine Rinas <katherinerinas@Katherine-Rinass-MacBook-Pro.local>
2018-01-19 18:54:33 -0700

    properly linked javascript page and tested the onkeyup function

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//dont forget to link this page with the html page!!!
+//user guesses	
+   //computer makes generated guesses
+   //wins-number of times the user is right
+   //losses-number or times the user is wrong
+   //guesses left (will count down to 0)
+   //guesses so far-the letters that the user typed (will probably use the document.onKeyup.ToLowerCase)
+
+   document.onkeyup = function(){
+   	alert("Working!")
+   }
