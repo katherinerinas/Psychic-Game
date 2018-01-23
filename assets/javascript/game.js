9b084f7be57e3bf09595e3d25576966da1419be4
Katherine Rinas <katherinerinas@Katherine-Rinass-MacBook-Pro.local>
2018-01-22 22:03:01 -0700

    took out console logs for aesthetic reasons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,13 +22,13 @@
    	var userguess = String.fromCharCode(event.keyCode).toLowerCase();
     guessesLeft--;
         	
-    console.log(userguess);
+   
 
    
    	var computerGuess = options[Math.floor(Math.random()*options.length)];
   
    	
-   	console.log(computerGuess);
+   	
 
    	
 
