cb401b36ede5e986bc4ec7e0dab592bf7cdd60cb
Katherine Rinas <katherinerinas@Katherine-Rinass-MacBook-Pro.local>
2018-02-05 19:17:20 -0700

    current guess shows

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,26 +13,23 @@
   var wins =0
   var losses=0
   var guessesLeft=15
-  var guessesSofar=0
+  var guessesSofar=[]
   
   
-  
-
-   
-   document.onkeyup = function(){
+  document.onkeyup = function(){
    	var userguess = String.fromCharCode(event.keyCode).toLowerCase();
     guessesLeft--;
-        	
    
+        	
+  
    
    	var computerGuess = options[Math.floor(Math.random()*options.length)];
   
-   	
+   	 
    	
 
    	if(userguess===computerGuess){
    		wins ++;
-      document.getElementById('xyz').play();
    		alert("You may be Psychic! wins: " + wins);
    	}
     //SEE IF POSSIBLE TO ADD AUDIO CLIP OF AMANDA SEYFRIED FROM MEAN GIRLS "ITS LIKE I HAVE ESPN OR SOMETHING!"
@@ -41,7 +38,6 @@
     	wins =0;
     	losses =0;
     	guessesLeft=15;
-    	  document.getElementById('xyz').play();
     alert("You might be a phony..Try again!");
 
     //SEE IF POSSIBLE TO ADD AUDIO CLIP OF BETTE MIDDLER FROM HOCUS POCUS "AHHHIVE LOST MY POWER!"
@@ -53,12 +49,15 @@
    	
    }
 
+   
+
    var html ="<p>(Press any letter to begin!)</p>" + 
    "<p><h3>Wins: " + wins +"</h3></p>"+
    "<p><h3>Losses: " + losses + "</h3></p>" +
    "<p><h3>Guesses Left: " + guessesLeft + "</h3></p>" +
-   "<p><h3>Your Guesses so far:" + userguess +"</h3></p>";
+   "<p><h3>Your Current Guess:" + userguess + "</h3></p>";
 
+   
    document.querySelector("#game").innerHTML = html;
 
 
